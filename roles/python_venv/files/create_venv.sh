f84f5c22cce929ac33dd7c187367761197ebe7e0
Jim Grady <jimgrady.jg@gmail.com>
2025-08-25 19:02:27 -0500

    Put contents of create_venv.sh in task YAML; update packages in install

--- roles/python_venv/files/create_venv.sh
+++
@@ -1,7 +0,0 @@
-#! /usr/bin/env bash
-
-python3 -m venv venv
-source venv/bin/activate
-python -m pip install --upgrade pip pip-tools
-python -m piptools compile --upgrade requirements.in
-python -m piptools sync requirements.txt
