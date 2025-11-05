39a84c45cff5eba8ade587313f28f918665e73e4
Jim Grady <jimgrady.jg@gmail.com>
2025-11-05 16:20:18 -0600

    Enhance running average (#13)

--- app/ui/resources_rc.py
+++ app/ui/resources_rc.py
@@ -40,6 +40,124 @@ a7 7 0 0 0-7 7 7\
  5-5 5 5 0 0 0-5\
 -5z\x22 fill=\x22#da16\
 36\x22/>\x0d\x0a</svg>\
+\x00\x00\x07>\
+<\
+?xml version=\x221.\
+0\x22 ?>\x0a\x0d<!DOCTYPE\
+ svg  PUBLIC '-/\
+/W3C//DTD SVG 1.\
+1//EN'  'http://\
+www.w3.org/Graph\
+ics/SVG/1.1/DTD/\
+svg11.dtd'>\x0a\x0d<!-\
+- Uploaded to: S\
+VG Repo, www.svg\
+repo.com, Genera\
+tor: SVG Repo Mi\
+xer Tools -->\x0a<s\
+vg width=\x22800px\x22\
+ height=\x22800px\x22 \
+viewBox=\x220 0 9.2\
+ 9.2\x22 style=\x22sha\
+pe-rendering:geo\
+metricPrecision;\
+ text-rendering:\
+geometricPrecisi\
+on; image-render\
+ing:optimizeQual\
+ity; fill-rule:e\
+venodd; clip-rul\
+e:evenodd\x22 versi\
+on=\x221.1\x22 xml:spa\
+ce=\x22preserve\x22 xm\
+lns=\x22http://www.\
+w3.org/2000/svg\x22\
+ xmlns:xlink=\x22ht\
+tp://www.w3.org/\
+1999/xlink\x22 xmln\
+s:xodm=\x22http://w\
+ww.corel.com/cor\
+eldraw/odm/2003\x22\
+>\x0a\x0d<defs>\x0a\x0d<styl\
+e type=\x22text/css\
+\x22>\x0d\x0a   <![CDATA[\
+\x0d\x0a    .fil0 {fil\
+l:none}\x0d\x0a    .fi\
+l3 {fill:#479DFF\
+}\x0d\x0a    .fil1 {fi\
+ll:#2554B0;fill-\
+rule:nonzero}\x0d\x0a \
+   .fil2 {fill:#\
+479DFF;fill-rule\
+:nonzero}\x0d\x0a   ]]\
+>\x0d\x0a  </style>\x0a\x0d<\
+/defs>\x0a\x0d<g id=\x22L\
+ayer_x0020_1\x22>\x0a\x0d\
+<rect class=\x22fil\
+0\x22 height=\x229.2\x22 \
+width=\x229.2\x22/>\x0a\x0d<\
+path class=\x22fil1\
+\x22 d=\x22M2.69 5.51c\
+-0.08,0 -0.15,-0\
+.07 -0.15,-0.15 \
+0,-0.08 0.07,-0.\
+15 0.15,-0.15l3.\
+82 0c0.08,0 0.14\
+,0.07 0.14,0.15 \
+0,0.08 -0.06,0.1\
+5 -0.14,0.15l-3.\
+82 0z\x22/>\x0a\x0d<path \
+class=\x22fil2\x22 d=\x22\
+M4.05 4.75c-0.06\
+,0.06 -0.15,0.06\
+ -0.21,0 -0.06,-\
+0.06 -0.06,-0.15\
+ 0,-0.21l0.56 -0\
+.56c0.05,-0.06 0\
+.15,-0.06 0.21,0\
+l0.38 0.39 0.93 \
+-0.93c0.06,-0.06\
+ 0.15,-0.06 0.21\
+,0 0.06,0.06 0.0\
+6,0.15 0,0.21l-1\
+.03 1.03c-0.06,0\
+.06 -0.15,0.06 -\
+0.21,0l-0.39 -0.\
+38 -0.45 0.45z\x22/\
+>\x0a\x0d<polygon clas\
+s=\x22fil3\x22 points=\
+\x226.19,3.38 6.13,\
+3.59 6.08,3.81 5\
+.92,3.65 5.76,3.\
+5 5.98,3.44 \x22/>\x0a\
+\x0d<path class=\x22fi\
+l2\x22 d=\x22M6.33 3.4\
+2l-0.11 0.43c-0.\
+02,0.08 -0.1,0.1\
+2 -0.18,0.1 -0.0\
+3,-0.01 -0.05,-0\
+.02 -0.07,-0.04l\
+-0.31 -0.31c-0.0\
+6,-0.06 -0.06,-0\
+.15 0,-0.21 0.02\
+,-0.02 0.05,-0.0\
+3 0.07,-0.04l0.4\
+2 -0.11c0.08,-0.\
+02 0.16,0.02 0.1\
+8,0.1 0.01,0.03 \
+0.01,0.06 0,0.08\
+z\x22/>\x0a\x0d<path clas\
+s=\x22fil1\x22 d=\x22M3.4\
+1 5.94c0,0.08 -0\
+.06,0.14 -0.14,0\
+.14 -0.09,0 -0.1\
+5,-0.06 -0.15,-0\
+.14l0 -2.68c0,-0\
+.08 0.06,-0.15 0\
+.15,-0.15 0.08,0\
+ 0.14,0.07 0.14,\
+0.15l0 2.68z\x22/>\x0a\
+\x0d</g>\x0a\x0d</svg>\
 \x00\x00\x0aG\
 <\
 ?xml version=\x221.\
@@ -477,6 +595,45 @@ th style=\x22fill:#\
 36,21.939,27.54,\
 15.026,27.54z\x22/>\
 \x0d\x0a</g>\x0d\x0a</svg>\
+\x00\x00\x02E\
+<\
+?xml version=\x221.\
+0\x22 encoding=\x22utf\
+-8\x22?><!-- Upload\
+ed to: SVG Repo,\
+ www.svgrepo.com\
+, Generator: SVG\
+ Repo Mixer Tool\
+s -->\x0d\x0a<svg widt\
+h=\x22800px\x22 height\
+=\x22800px\x22 viewBox\
+=\x220 0 24 24\x22 fil\
+l=\x22none\x22 xmlns=\x22\
+http://www.w3.or\
+g/2000/svg\x22>\x0d\x0a<p\
+ath d=\x22M20 14V7C\
+20 5.34315 18.65\
+69 4 17 4H12M20 \
+14L13.5 20M20 14\
+H15.5C14.3954 14\
+ 13.5 14.8954 13\
+.5 16V20M13.5 20\
+H7C5.34315 20 4 \
+18.6569 4 17V12\x22\
+ stroke=\x22#000000\
+\x22 stroke-width=\x22\
+2\x22 stroke-lineca\
+p=\x22round\x22 stroke\
+-linejoin=\x22round\
+\x22/>\x0d\x0a<path d=\x22M7\
+ 4V7M7 10V7M7 7H\
+4M7 7H10\x22 stroke\
+=\x22#000000\x22 strok\
+e-width=\x222\x22 stro\
+ke-linecap=\x22roun\
+d\x22 stroke-linejo\
+in=\x22round\x22/>\x0d\x0a</\
+svg>\
 \x00\x00\x03\x8a\
 <\
 ?xml version=\x221.\
@@ -621,6 +778,76 @@ ll=\x22#000000\x22/>\x0d\x0a\
  3H15Z\x22 fill=\x22#0\
 00000\x22/>\x0d\x0a</svg>\
 \
+\x00\x00\x044\
+<\
+?xml version=\x221.\
+0\x22 encoding=\x22utf\
+-8\x22?><!-- Upload\
+ed to: SVG Repo,\
+ www.svgrepo.com\
+, Generator: SVG\
+ Repo Mixer Tool\
+s -->\x0a<svg fill=\
+\x22#000000\x22 width=\
+\x22800px\x22 height=\x22\
+800px\x22 viewBox=\x22\
+0 0 24 24\x22 id=\x22n\
+ote-edit\x22 data-n\
+ame=\x22Flat Line\x22 \
+xmlns=\x22http://ww\
+w.w3.org/2000/sv\
+g\x22 class=\x22icon f\
+lat-line\x22><path \
+id=\x22secondary\x22 d\
+=\x22M18,4H15V5a1,1\
+,0,0,1-1,1H10A1,\
+1,0,0,1,9,5V4H6A\
+1,1,0,0,0,5,5V20\
+a1,1,0,0,0,1,1H1\
+8a1,1,0,0,0,1-1V\
+5A1,1,0,0,0,18,4\
+Zm-3.67,8.29-3.1\
+8,3.18-2.48.36L9\
+,13.35l3.18-3.18\
+a1.5,1.5,0,0,1,2\
+.12,2.12Z\x22 style\
+=\x22fill: rgb(44, \
+169, 188); strok\
+e-width: 2;\x22></p\
+ath><path id=\x22pr\
+imary\x22 d=\x22M14,6H\
+10A1,1,0,0,1,9,5\
+V4a1,1,0,0,1,1-1\
+h4a1,1,0,0,1,1,1\
+V5A1,1,0,0,1,14,\
+6Zm.33,6.29a1.51\
+,1.51,0,0,0,0-2.\
+12h0a1.51,1.51,0\
+,0,0-2.12,0L9,13\
+.35l-.36,2.48,2.\
+48-.36Z\x22 style=\x22\
+fill: none; stro\
+ke: rgb(0, 0, 0)\
+; stroke-linecap\
+: round; stroke-\
+linejoin: round;\
+ stroke-width: 2\
+;\x22></path><path \
+id=\x22primary-2\x22 d\
+ata-name=\x22primar\
+y\x22 d=\x22M18,4a1,1,\
+0,0,1,1,1V20a1,1\
+,0,0,1-1,1H6a1,1\
+,0,0,1-1-1V5A1,1\
+,0,0,1,6,4\x22 styl\
+e=\x22fill: none; s\
+troke: rgb(0, 0,\
+ 0); stroke-line\
+cap: round; stro\
+ke-linejoin: rou\
+nd; stroke-width\
+: 2;\x22></path></s\
+vg>\
 \x00\x00\x07\xc3\
 <\
 ?xml version=\x221.\
@@ -748,21 +975,144 @@ ill=\x22#231C1C\x22 />\
 6h-85.76z\x22 fill=\
 \x22#231C1C\x22 /></sv\
 g>\
+\x00\x00\x07>\
+<\
+?xml version=\x221.\
+0\x22 ?>\x0a\x0d<!DOCTYPE\
+ svg  PUBLIC '-/\
+/W3C//DTD SVG 1.\
+1//EN'  'http://\
+www.w3.org/Graph\
+ics/SVG/1.1/DTD/\
+svg11.dtd'>\x0a\x0d<!-\
+- Uploaded to: S\
+VG Repo, www.svg\
+repo.com, Genera\
+tor: SVG Repo Mi\
+xer Tools -->\x0a<s\
+vg width=\x22800px\x22\
+ height=\x22800px\x22 \
+viewBox=\x220 0 9.2\
+ 9.2\x22 style=\x22sha\
+pe-rendering:geo\
+metricPrecision;\
+ text-rendering:\
+geometricPrecisi\
+on; image-render\
+ing:optimizeQual\
+ity; fill-rule:e\
+venodd; clip-rul\
+e:evenodd\x22 versi\
+on=\x221.1\x22 xml:spa\
+ce=\x22preserve\x22 xm\
+lns=\x22http://www.\
+w3.org/2000/svg\x22\
+ xmlns:xlink=\x22ht\
+tp://www.w3.org/\
+1999/xlink\x22 xmln\
+s:xodm=\x22http://w\
+ww.corel.com/cor\
+eldraw/odm/2003\x22\
+>\x0a\x0d<defs>\x0a\x0d<styl\
+e type=\x22text/css\
+\x22>\x0d\x0a   <![CDATA[\
+\x0d\x0a    .fil0 {fil\
+l:none}\x0d\x0a    .fi\
+l3 {fill:#479DFF\
+}\x0d\x0a    .fil1 {fi\
+ll:#2554B0;fill-\
+rule:nonzero}\x0d\x0a \
+   .fil2 {fill:#\
+479DFF;fill-rule\
+:nonzero}\x0d\x0a   ]]\
+>\x0d\x0a  </style>\x0a\x0d<\
+/defs>\x0a\x0d<g id=\x22L\
+ayer_x0020_1\x22>\x0a\x0d\
+<rect class=\x22fil\
+0\x22 height=\x229.2\x22 \
+width=\x229.2\x22/>\x0a\x0d<\
+path class=\x22fil1\
+\x22 d=\x22M2.69 5.51c\
+-0.08,0 -0.15,-0\
+.07 -0.15,-0.15 \
+0,-0.08 0.07,-0.\
+15 0.15,-0.15l3.\
+82 0c0.08,0 0.14\
+,0.07 0.14,0.15 \
+0,0.08 -0.06,0.1\
+5 -0.14,0.15l-3.\
+82 0z\x22/>\x0a\x0d<path \
+class=\x22fil2\x22 d=\x22\
+M4.05 4.75c-0.06\
+,0.06 -0.15,0.06\
+ -0.21,0 -0.06,-\
+0.06 -0.06,-0.15\
+ 0,-0.21l0.56 -0\
+.56c0.05,-0.06 0\
+.15,-0.06 0.21,0\
+l0.38 0.39 0.93 \
+-0.93c0.06,-0.06\
+ 0.15,-0.06 0.21\
+,0 0.06,0.06 0.0\
+6,0.15 0,0.21l-1\
+.03 1.03c-0.06,0\
+.06 -0.15,0.06 -\
+0.21,0l-0.39 -0.\
+38 -0.45 0.45z\x22/\
+>\x0a\x0d<polygon clas\
+s=\x22fil3\x22 points=\
+\x226.19,3.38 6.13,\
+3.59 6.08,3.81 5\
+.92,3.65 5.76,3.\
+5 5.98,3.44 \x22/>\x0a\
+\x0d<path class=\x22fi\
+l2\x22 d=\x22M6.33 3.4\
+2l-0.11 0.43c-0.\
+02,0.08 -0.1,0.1\
+2 -0.18,0.1 -0.0\
+3,-0.01 -0.05,-0\
+.02 -0.07,-0.04l\
+-0.31 -0.31c-0.0\
+6,-0.06 -0.06,-0\
+.15 0,-0.21 0.02\
+,-0.02 0.05,-0.0\
+3 0.07,-0.04l0.4\
+2 -0.11c0.08,-0.\
+02 0.16,0.02 0.1\
+8,0.1 0.01,0.03 \
+0.01,0.06 0,0.08\
+z\x22/>\x0a\x0d<path clas\
+s=\x22fil1\x22 d=\x22M3.4\
+1 5.94c0,0.08 -0\
+.06,0.14 -0.14,0\
+.14 -0.09,0 -0.1\
+5,-0.06 -0.15,-0\
+.14l0 -2.68c0,-0\
+.08 0.06,-0.15 0\
+.15,-0.15 0.08,0\
+ 0.14,0.07 0.14,\
+0.15l0 2.68z\x22/>\x0a\
+\x0d</g>\x0a\x0d</svg>\
 "
 
 qt_resource_name = b"\
-\x00\x09\
-\x0alxC\
-\x00r\
-\x00e\x00s\x00o\x00u\x00r\x00c\x00e\x00s\
 \x00\x05\
 \x00o\xa6S\
 \x00i\
 \x00c\x00o\x00n\x00s\
+\x00\x09\
+\x0alxC\
+\x00r\
+\x00e\x00s\x00o\x00u\x00r\x00c\x00e\x00s\
 \x00\x10\
 \x08\x12\xa3'\
 \x00m\
 \x00e\x00d\x00i\x00a\x00-\x00r\x00e\x00c\x00o\x00r\x00d\x00.\x00s\x00v\x00g\
+\x00\x11\
+\x05\x8d\xf6\x07\
+\x00g\
+\x00r\x00a\x00p\x00h\x00-\x00d\x00u\x00o\x00t\x00o\x00n\x00e\x00.\x00s\x00v\x00g\
+\
 \x00\x16\
 \x00g$\xe7\
 \x00f\
@@ -776,6 +1126,10 @@ qt_resource_name = b"\
 \x07G\x89g\
 \x00s\
 \x00t\x00o\x00p\x00-\x00b\x00u\x00t\x00t\x00o\x00n\x00.\x00s\x00v\x00g\
+\x00\x0c\
+\x0aT\xb2g\
+\x00a\
+\x00d\x00d\x00-\x00n\x00o\x00t\x00e\x00.\x00s\x00v\x00g\
 \x00\x0e\
 \x05\xd6\xce\xc7\
 \x00m\
@@ -788,6 +1142,10 @@ qt_resource_name = b"\
 \x09\x81\x19'\
 \x00m\
 \x00e\x00d\x00i\x00a\x00-\x00p\x00a\x00u\x00s\x00e\x00.\x00s\x00v\x00g\
+\x00\x0c\
+\x08A\x12g\
+\x00n\
+\x00e\x00w\x00-\x00n\x00o\x00t\x00e\x00.\x00s\x00v\x00g\
 \x00\x1b\
 \x0c\xf7%\x07\
 \x00t\
@@ -796,28 +1154,38 @@ qt_resource_name = b"\
 "
 
 qt_resource_struct = b"\
-\x00\x00\x00\x00\x00\x02\x00\x00\x00\x01\x00\x00\x00\x01\
+\x00\x00\x00\x00\x00\x02\x00\x00\x00\x02\x00\x00\x00\x01\
 \x00\x00\x00\x00\x00\x00\x00\x00\
-\x00\x00\x00\x00\x00\x02\x00\x00\x00\x01\x00\x00\x00\x02\
+\x00\x00\x00\x00\x00\x02\x00\x00\x00\x01\x00\x00\x00\x0f\
 \x00\x00\x00\x00\x00\x00\x00\x00\
-\x00\x00\x00\x18\x00\x02\x00\x00\x00\x08\x00\x00\x00\x03\
+\x00\x00\x00\x10\x00\x02\x00\x00\x00\x01\x00\x00\x00\x03\
 \x00\x00\x00\x00\x00\x00\x00\x00\
+\x00\x00\x00\x00\x00\x02\x00\x00\x00\x0b\x00\x00\x00\x04\
+\x00\x00\x00\x00\x00\x00\x00\x00\
+\x00\x00\x00v\x00\x00\x00\x00\x00\x01\x00\x00\x09\x04\
+\x00\x00\x01\x99\x82\xe1cm\
 \x00\x00\x00N\x00\x00\x00\x00\x00\x01\x00\x00\x01\xc2\
+\x00\x00\x01\x9aJr\xb7Y\
+\x00\x00\x01\x06\x00\x00\x00\x00\x00\x01\x00\x00&6\
 \x00\x00\x01\x99\x82\xe1cm\
-\x00\x00\x00\xc0\x00\x00\x00\x00\x00\x01\x00\x00\x1c\xab\
-\x00\x00\x01\x99\x82\xe1cm\
-\x00\x00\x00\x9c\x00\x00\x00\x00\x00\x01\x00\x00\x198\
+\x00\x00\x00\xc4\x00\x00\x00\x00\x00\x01\x00\x00 z\
 \x00\x00\x01\x99\xf8\x18*\x96\
 \x00\x00\x00(\x00\x00\x00\x00\x00\x01\x00\x00\x00\x00\
 \x00\x00\x01\x99\xf8\x18*\x96\
-\x00\x00\x00\x80\x00\x00\x00\x00\x00\x01\x00\x00\x0c\x0d\
+\x00\x00\x01n\x00\x00\x00\x00\x00\x01\x00\x00.\xc1\
+\x00\x00\x01\x9aTX@\xaa\
+\x00\x00\x00\xa8\x00\x00\x00\x00\x00\x01\x00\x00\x13O\
 \x00\x00\x01\x99\xf8\x18*\x96\
-\x00\x00\x01\x04\x00\x00\x00\x00\x00\x01\x00\x00#!\
+\x00\x00\x01J\x00\x00\x00\x00\x00\x01\x00\x00,\xac\
 \x00\x00\x01\x99\xf8\x18*\x96\
-\x00\x00\x00\xe2\x00\x00\x00\x00\x00\x01\x00\x00 9\
+\x00\x00\x00\xe8\x00\x00\x00\x00\x00\x01\x00\x00#\xed\
+\x00\x00\x01\x9aTY<F\
+\x00\x00\x01(\x00\x00\x00\x00\x00\x01\x00\x00)\xc4\
 \x00\x00\x01\x99\x82\xe1cm\
-\x00\x00\x01(\x00\x00\x00\x00\x00\x01\x00\x00%6\
+\x00\x00\x01\x8c\x00\x00\x00\x00\x00\x01\x00\x002\xf9\
 \x00\x00\x01\x99\x82\xe1cm\
+\x00\x00\x00N\x00\x00\x00\x00\x00\x01\x00\x00:\xc0\
+\x00\x00\x01\x9aJr\xb7Y\
 "
 
 def qInitResources():
